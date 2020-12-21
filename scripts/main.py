6924541f913459f9933378ce71591fd0dc006e65
Sai Prashanth T S <saiprashanthts@gmail.com>
2020-12-21 19:06:19 +0530

    added all logics to main.py except load

--- scripts/main.py
+++ scripts/main.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from utils import create_postgres_connection, format_print, logging, load_tables, read_file, calculate_time
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@calculate_time
+def run(env):
+    if env == "dev":
+        logger.info("This is used to load data into RDS instance")
+        format_print("This is used to load data into RDS instance", type_of_message="heading")
+
+        # table details
+        table_details = load_tables('iris')
+        file_name = table_details['source_file_name']
+        file_extension = file_name.split('.')[1]
+        table_name = table_details['target_table_name']
+        logger.info("Name of the source file is {}".format(file_name))
+        logger.info("Name of the target table is {}".format(table_name))
+
+        # load data as pandas dataframe
+        data = read_file(file_name, filetype=file_extension)
+        logger.info("Sample data is shown below for your reference")
+        logger.info(data.head())
+
+        # connection to postgres
+    else:
+        print("First complete the development in dev environment and then move on :) ")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="to load data to postgres",
+        prog="main.py"
+    )
+    parser.add_argument("--env", "-n", help="Name of the environment",
+                        dest="env")
+    args = parser.parse_args()
+    # logger object
+    logger = logging()
+    logger.info("We are loading data to {} environment".format(args.env))
+    logger.info("Loading data process begins now".format(args.env))
+    run(args.env)
+    logger.info("Loading data process ends now".format(args.env))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
